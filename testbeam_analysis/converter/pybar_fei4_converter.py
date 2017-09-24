25ba2d37013e0331324f99843d46fc7777c7b15d
DavidLP <pohl@physik.uni-bonn.de>
2017-09-24 15:57:22 +0200

    ENH: multiple raw data files of same run

--- testbeam_analysis/converter/pybar_fei4_converter.py
+++ testbeam_analysis/converter/pybar_fei4_converter.py
@@ -7,9 +7,10 @@ An installation of pyBAR is required: https://silab-redmine.physik.uni-bonn.de/p
 """
 
 import logging
+from multiprocessing import Pool
+
 import numpy as np
 import tables as tb
-from multiprocessing import Pool
 
 from pybar.analysis import analysis_utils
 from pybar.analysis.analyze_raw_data import AnalyzeRawData
@@ -27,11 +28,20 @@ def analyze_raw_data(input_file, trigger_data_format):  # FE-I4 raw data analysi
     input_file : pytables file
     output_file_hits : pytables file
     '''
-    with AnalyzeRawData(raw_data_file=input_file, create_pdf=True) as analyze_raw_data:
-        #analyze_raw_data.align_at_trigger_number = True  # if trigger number is at the beginning of each event activate this for event alignment
+
+    # Set output file name if multiple input file names
+    analyzed_data_file = None
+    if isinstance(input_file, list):
+        analyzed_data_file = input_file[0][:-3] + '_interpreted.h5'
+
+    with AnalyzeRawData(raw_data_file=input_file,
+                        analyzed_data_file=analyzed_data_file,
+                        create_pdf=True) as analyze_raw_data:
+        # analyze_raw_data.align_at_trigger_number = True  # if trigger number is at the beginning of each event activate this for event alignment
         analyze_raw_data.use_trigger_time_stamp = False  # the trigger number is a time stamp
         analyze_raw_data.trigger_data_format = trigger_data_format
         analyze_raw_data.use_tdc_word = False
+        analyze_raw_data.create_tot_pixel_hist = False
         analyze_raw_data.create_hit_table = True
         analyze_raw_data.create_meta_event_index = True
         analyze_raw_data.create_trigger_error_hist = True
@@ -56,8 +66,8 @@ def process_dut(raw_data_file, trigger_data_format=0, do_corrections=False):
     ''' Process and format raw data.
     Parameters
     ----------
-    raw_data_file : string
-        file with raw data
+    raw_data_file : string, iterable
+        file or files with raw data from one run
     fix_trigger_number : bool
         activate trigger number fixing
 
@@ -73,8 +83,14 @@ def process_dut(raw_data_file, trigger_data_format=0, do_corrections=False):
         fix_trigger_number, fix_event_number = False, False
 
     analyze_raw_data(raw_data_file, trigger_data_format=trigger_data_format)
-    ret_value = align_events(raw_data_file[:-3] + '_interpreted.h5', raw_data_file[:-3] + '_event_aligned.h5', fix_trigger_number=fix_trigger_number, fix_event_number=fix_event_number)
-    format_hit_table(raw_data_file[:-3] + '_event_aligned.h5', raw_data_file[:-3] + '_aligned.h5')
+    if isinstance(raw_data_file, list):
+        raw_data_file = raw_data_file[0]
+    ret_value = align_events(raw_data_file[:-3] + '_interpreted.h5',
+                             raw_data_file[:-3] + '_event_aligned.h5',
+                             fix_trigger_number=fix_trigger_number,
+                             fix_event_number=fix_event_number)
+    format_hit_table(raw_data_file[:-3] + '_event_aligned.h5',
+                     raw_data_file[:-3] + '_aligned.h5')
     return ret_value
 
 
@@ -97,58 +113,64 @@ def align_events(input_file, output_file, fix_event_number=True, fix_trigger_num
         hit_table = in_file_h5.root.Hits
         jumps = []  # variable to determine the jumps in the event-number to trigger-number offset
         n_fixed_hits = 0  # events that were fixed
-    
+
         with tb.open_file(output_file, 'w') as out_file_h5:
             hit_table_description = data_struct.HitInfoTable().columns.copy()
-            hit_table_out = out_file_h5.create_table(out_file_h5.root, name='Hits', description=hit_table_description, title='Selected hits for test beam analysis', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False), chunkshape=(chunk_size,))
- 
+            hit_table_out = out_file_h5.create_table(out_file_h5.root,
+                                                     name='Hits',
+                                                     description=hit_table_description,
+                                                     title='Selected hits for test beam analysis',
+                                                     filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False),
+                                                     expectedrows=chunk_size,
+                                                     chunkshape=(chunk_size,))
+
             # Correct hit event number
             for hits, _ in analysis_utils.data_aligned_at_events(hit_table, chunk_size=chunk_size):
- 
+
                 if not np.all(np.diff(hits['event_number']) >= 0):
                     raise RuntimeError('The event number does not always increase. This data cannot be used like this!')
- 
+
                 if fix_trigger_number is True:
                     selection = np.logical_or((hits['trigger_status'] & 0b00000001) == 0b00000001,
                                               (hits['event_status'] & 0b0000000000000010) == 0b0000000000000010)
                     selected_te_hits = np.where(selection)[0]  # select both events with and without hit that have trigger error flag set
- 
+
 #                     assert selected_te_hits[0] > 0
                     tmp_trigger_number = hits['trigger_number'].astype(np.int32)
- 
+
                     # save trigger and event number for plotting correlation between trigger number and event number
                     event_number, trigger_number = hits['event_number'].copy(), hits['trigger_number'].copy()
- 
+
                     hits['trigger_number'][0] = 0
- 
+
                     offset = (hits['trigger_number'][selected_te_hits] - hits['trigger_number'][selected_te_hits - 1] - hits['event_number'][selected_te_hits] + hits['event_number'][selected_te_hits - 1]).astype(np.int32)  # save jumps in trigger number
                     offset_tot = np.cumsum(offset)
- 
+
                     offset_tot[offset_tot > 32768] = np.mod(offset_tot[offset_tot > 32768], 32768)
                     offset_tot[offset_tot < -32768] = np.mod(offset_tot[offset_tot < -32768], 32768)
- 
+
                     for start_hit_index in range(len(selected_te_hits)):
                         start_hit = selected_te_hits[start_hit_index]
                         stop_hit = selected_te_hits[start_hit_index + 1] if start_hit_index < (len(selected_te_hits) - 1) else None
                         tmp_trigger_number[start_hit:stop_hit] -= offset_tot[start_hit_index]
- 
+
                     tmp_trigger_number[tmp_trigger_number >= 32768] = np.mod(tmp_trigger_number[tmp_trigger_number >= 32768], 32768)
                     tmp_trigger_number[tmp_trigger_number < 0] = 32768 - np.mod(np.abs(tmp_trigger_number[tmp_trigger_number < 0]), 32768)
- 
+
                     hits['trigger_number'] = tmp_trigger_number
- 
+
                 selected_hits = hits[(hits['event_status'] & 0b0000100000000000) == 0b0000000000000000]  # select not empty events
- 
+
                 if fix_event_number is True:
                     selector = (selected_hits['event_number'] != (np.divide(selected_hits['event_number'] + 1, 32768) * 32768 + selected_hits['trigger_number'] - 1))
                     n_fixed_hits += np.count_nonzero(selector)
                     selector = selected_hits['event_number'] > selected_hits['trigger_number']
                     selected_hits['event_number'] = np.divide(selected_hits['event_number'] + 1, 32768) * 32768 + selected_hits['trigger_number'] - 1
                     selected_hits['event_number'][selector] = np.divide(selected_hits['event_number'][selector] + 1, 32768) * 32768 + 32768 + selected_hits['trigger_number'][selector] - 1
- 
+
 #                 FIX FOR DIAMOND:
 #                 selected_hits['event_number'] -= 1  # FIX FOR DIAMOND EVENT OFFSET
- 
+
                 hit_table_out.append(selected_hits)
 
         jumps = np.unique(np.array(jumps))
@@ -186,10 +208,10 @@ def format_hit_table(input_file, output_file):
 if __name__ == "__main__":
 
     # Input raw data file names
-#     raw_data_files = [r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\138_proto_9_ext_trigger_scan.h5',  # the first DUT is the master reference DUT
-#                       r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\64_lfcmos_3_ext_trigger_scan.h5',
-#                       r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\102_scc_167_ext_trigger_scan.h5'
-#                       ]
+    #     raw_data_files = [r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\138_proto_9_ext_trigger_scan.h5',  # the first DUT is the master reference DUT
+    #                       r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\64_lfcmos_3_ext_trigger_scan.h5',
+    #                       r'C:\Users\DavidLP\Desktop\TB\RUN_20\Raw_Data\102_scc_167_ext_trigger_scan.h5'
+    #                       ]
     raw_data_files = [r'C:\Users\DavidLP\Desktop\TB\RUN_30\Raw_Data\160_proto_9_ext_trigger_scan.h5',  # the first DUT is the master reference DUT
                       r'C:\Users\DavidLP\Desktop\TB\RUN_30\Raw_Data\7_lfcmos_3_ext_trigger_scan.h5',
                       r'C:\Users\DavidLP\Desktop\TB\RUN_30\Raw_Data\124_scc_167_ext_trigger_scan.h5'
