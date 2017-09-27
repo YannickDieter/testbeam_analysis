4abbf2718a2f65ce08458ee7c2b47dbb3bb3e5c2
DavidLP <David-Leon.Pohl@rub.de>
2017-09-27 19:31:52 +0200

    PRJ: try fix autodoc

---
+++ scripts/create_doc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$MINICONDA_OS" == "linux" ]; then
+  sphinx-build docs docs/_build/html;
+  eval "$(ssh-agent -s)"; touch docs/key; chmod 0600 docs/key;
+  openssl aes-256-cbc -K $encrypted_ba29598036fd_key -iv $encrypted_ba29598036fd_iv -in docs/key.enc -out docs/key -d && ssh-add docs/key;
+  git config --global user.email "builds@travis-ci.com";
+  git config --global user.name "Travis CI";
+  git remote set-url --push origin "git@github.com:$TRAVIS_REPO_SLUG";
+  export ${!TRAVIS*};
+  sphinx-versioning push -r development -w master -w development -b docs gh-pages .;
+fi
