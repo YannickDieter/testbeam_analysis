ENH: copy test tools from pyBAR, fixing several issues with recarrays, dtypes, broadcast, etc.

testbeam_analysis/tools/analysis_utils.py
@@ -371,7 +371,7 @@ def get_data_in_event_range(array, event_start=None, event_stop=None, assume_sor
         else:
             if event_number[-1] < event_stop:
                 max_index_data = event_number.shape[0]
-            else: 
+            else:
                 max_index_data = np.argmax(event_number >= event_stop)
 
         if min_index_data < 0:
@@ -669,10 +669,10 @@ def get_mean_efficiency(array_pass, array_total, method=0):
           4: Use a special Binomial efficiency fit. Gives best mean efficiency but unphysical symmetric error
              bars. Further info: https://root.cern.ch/doc/master/classTBinomialEfficiencyFitter.html
     '''
-    
+
     n_bins = np.ma.count(array_pass)
     logging.info('Calculate the mean efficiency from %d pixels', n_bins)
-    
+
     if method == 0:
         def weighted_avg_and_std(values, weights):  # http://stackoverflow.com/questions/2413522/weighted-standard-deviation-in-numpy
             average = np.average(values, weights=weights)
@@ -686,7 +686,7 @@ def get_mean_efficiency(array_pass, array_total, method=0):
             from ROOT import TH1D, TEfficiency, TF1, TBinomialEfficiencyFitter
         except ImportError:
             raise RuntimeError('To use these method you have to install CERN ROOT with python bindings.')
-    
+
         # Convert not masked numpy array values to 1D ROOT double histogram
         def fill_1d_root_hist(array, name):
             length = np.ma.count(array_pass)
@@ -694,7 +694,7 @@ def get_mean_efficiency(array_pass, array_total, method=0):
             for index, value in enumerate(np.ma.compressed(array).ravel()):
                 root_hist.SetBinContent(index, value)
             return root_hist
-        
+
         if method == 1:
             # The following combines all pixel and gives correct
             # statistical errors but does not take the systematic
@@ -710,7 +710,7 @@ def get_mean_efficiency(array_pass, array_total, method=0):
             # The following fits the efficiency with a constant but
             # it gives symmetric error bars, thus unphysical results
             # This is not understood yet and thus not used
-            
+
             # Convert numpy array to ROOT hists
             hist_pass = fill_1d_root_hist(array_pass, 'h_pass')
             hist_total = fill_1d_root_hist(array_total, 'h_total')
@@ -937,7 +937,7 @@ def hough_transform(img, theta_res=1.0, rho_res=1.0, return_edges=False):
     cos_t = np.cos(thetas)
     sin_t = np.sin(thetas)
 
-    accumulator = np.zeros((rhos.size, thetas.size), dtype=np.int)
+    accumulator = np.zeros((rhos.size, thetas.size), dtype=np.int32)
     y_idxs, x_idxs = np.nonzero(img)
 
     @njit
@@ -968,10 +968,10 @@ def hough_transform(img, theta_res=1.0, rho_res=1.0, return_edges=False):
 
 def get_data(path, output=None, fail_on_overwrite=False):
     ''' Downloads data (eg. for examples, fixtures).
-    
+
         Uses data in a public scibo folder. If you want
         write access contact the maintainer.
-        
+
         Parameters
         ----------
         path : string
@@ -1023,5 +1023,5 @@ def get_data(path, output=None, fail_on_overwrite=False):
                        filename=os.path.join(output_path, output))
     elif fail_on_overwrite:
         raise RuntimeError('The files %s exists already', output)
-    
+
     return os.path.join(output_path, output)

testbeam_analysis/tools/data_selection.py
@@ -167,10 +167,10 @@ def select_hits(hit_file, max_hits=None, condition=None, track_quality=None,
                         # Calculate number of hits to add for this chunk
                         # Fraction of hits to add per chunk
                         hit_fraction = max_hits / float(total_hits)
-                        sel = np.ceil(np.linspace(0, hits.shape[0],
+                        sel = np.ceil(np.linspace(0,
+                                                  hits.shape[0],
                                                   int(hit_fraction * n_hits),
-                                                  endpoint=False)).astype(
-                                                      np.int)
+                                                  endpoint=False)).astype(np.int32)
                         sel = sel[sel < hits.shape[0]]
                         hits = hits[sel]
 

testbeam_analysis/tools/residuals.py
@@ -74,9 +74,9 @@ if __name__ == '__main__':
     hits_mc_2 = tracks_mc[:, 0] + (tracks_mc[:, 1] - tracks_mc[:, 0]) / 2.  # real hit position in DUT
 
     # Digitized hit info (firing pixels) for all 3 planes
-    hits_reco_1 = ((tracks_mc[:, 0] - plane_offsets[0]) / telescope_pitch).astype(np.int)
-    hits_reco_2 = ((tracks_mc[:, 0] + (tracks_mc[:, 1] - tracks_mc[:, 0]) / 2.) / DUT_pitch).astype(np.int)
-    hits_reco_3 = ((tracks_mc[:, 1] - plane_offsets[2]) / telescope_pitch).astype(np.int)
+    hits_reco_1 = ((tracks_mc[:, 0] - plane_offsets[0]) / telescope_pitch).astype(np.int32)
+    hits_reco_2 = ((tracks_mc[:, 0] + (tracks_mc[:, 1] - tracks_mc[:, 0]) / 2.) / DUT_pitch).astype(np.int32)
+    hits_reco_3 = ((tracks_mc[:, 1] - plane_offsets[2]) / telescope_pitch).astype(np.int32)
     # Correct for int conversion that int(]-1 .. 1[) = 0
     hits_reco_1[tracks_mc[:, 0] - plane_offsets[0] < 0] = hits_reco_1[tracks_mc[:, 0] - plane_offsets[0] < 0] - 1
     hits_reco_2[tracks_mc[:, 0] + (tracks_mc[:, 1] - tracks_mc[:, 0]) / 2. < 0] = hits_reco_2[tracks_mc[:, 0] + (tracks_mc[:, 1] - tracks_mc[:, 0]) / 2. < 0] - 1

testbeam_analysis/tools/simulate_data.py
@@ -785,10 +785,10 @@ class SimulateData(object):
         # Calculate the number of tracks per event
         if self.tracks_per_event_sigma > 0:
             n_tracks_per_event = np.random.normal(
-                self.tracks_per_event, self.tracks_per_event_sigma, n_events).astype(np.int)
+                self.tracks_per_event, self.tracks_per_event_sigma, n_events).astype(np.int32)
         else:
             n_tracks_per_event = np.ones(
-                n_events, dtype=np.int) * self.tracks_per_event
+                n_events, dtype=np.int32) * self.tracks_per_event
         # One cannot have less than 0 tracks per event, this will be triggered
         # events without a track
         n_tracks_per_event[n_tracks_per_event < 0] = 0